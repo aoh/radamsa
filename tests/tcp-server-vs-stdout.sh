514cc45762d7c5eb94c3be7ef72784438cfb402e
Aki Helin <aki.helin@iki.fi>
2011-09-03 22:30:03 +0300

    using owl instead of netcat to read connections in tcp server mode test (to differing netcat version issues and possibly track down a bug)

--- tests/tcp-server-vs-stdout.sh
+++ tests/tcp-server-vs-stdout.sh
@@ -7,7 +7,7 @@ fail() {
 
 mkdir -p tmp
 
-NFILES=70
+NFILES=100
 SEED=$RANDOM
 
 $@ -o - --seed $SEED -n $NFILES *.l > tmp/stdout-$$
@@ -18,11 +18,27 @@ echo -n "" > tmp/tcp-$$
 
 echo -n "("
 
-for foo in $(ol -e "(iota 0 1 $NFILES)")
-do
-   echo -n "-"
-   nc localhost 31337 >> tmp/tcp-$$
-done
+## read the connections using owl, which is already a dependency and doesn't seem to 
+## misbehave occasionally as netcat did (not sure why)
+
+echo "
+(define (stderr-data fd)
+   (let ((block (interact fd 'input)))
+      (if (and block (not (eof? block)))
+         (begin
+            (mail stderr block)
+            (stderr-data fd)))))
+(let loop ((n 0))
+   (if (< n $NFILES)
+      (let ((fd (open-connection (vector 127 0 0 1) 31337)))
+         (if fd
+            (begin
+               (mail stdout 45)
+               (flush-port stdout)
+               (stderr-data fd)
+               (loop (+ n 1)))
+            (loop n)))))
+" | ol -q 2> tmp/tcp-$$
 
 diff -q tmp/stdout-$$ tmp/tcp-$$ || fail "tcp server output differs from stdout output"
 
