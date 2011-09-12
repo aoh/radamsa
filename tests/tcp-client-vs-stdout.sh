5fcf6d236d3153ae174eb8db108ff4df8ef2539d
Aki Helin <aki.helin@iki.fi>
2011-09-12 17:29:20 +0300

    testing individual files in the tcp client test

--- tests/tcp-client-vs-stdout.sh
+++ tests/tcp-client-vs-stdout.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# check that what is sent to host/port over tcp matches what comes out of stdout
+
 fail() {
    echo "ERROR - " $@
    exit 1
@@ -8,55 +10,24 @@ fail() {
 mkdir -p tmp
 
 NFILES=10
-SEED=$RANDOM
-
-$@ -o - --seed $SEED -n $NFILES *.l > tmp/stdout-$$
-
-# check that we did get something
-test -s tmp/stdout-$$ || fail "didn't make anything"
-
-# this will retry connections until successful, so must be ok to start before listening
-$@ -o 127.0.0.1:31337 --seed $SEED -n $NFILES *.l &
-
-echo -n "" > tmp/tcp-$$
 
 echo -n "("
+for foo in $(ol -e "(iota 0 1 $NFILES)")
+do
+   SEED=$RANDOM
+   echo -n "-"
+   $@ --seed $SEED *.l > tmp/stdout-$$
+   $@ -o 127.0.0.1:31337 --seed $SEED *.l & 
+   echo "(mail stdout (force (port->byte-stream (interact (open-socket 31337) 'accept)))) (interact stdout 'sync)" \
+      | ol -q \
+      > tmp/tcp-$$
+   #strace nc -l -p 31337 > tmp/tcp-$$ 2>tmp/nc-$$                 # should be the same
 
-#for foo in $(ol -e "(iota 0 1 $NFILES)")
-#do
-#   echo -n "-"
-#   nc -l -p 31337 >> tmp/tcp-$$
-#done
-
-echo "
-(define (stderr-data fd)
-   (let ((block (interact fd 'input)))
-      (if (and block (not (eof? block)))
-         (begin
-            (mail stderr block)
-            (stderr-data fd)))))
-(define sock (open-socket 31337))
-(let loop ((n 0))
-   (if (< n $NFILES)
-      (let ((fd (interact sock 'accept)))
-         (if fd
-            (begin
-               (mail stdout 45)
-               (flush-port stdout)
-               (stderr-data fd)
-               (loop (+ n 1)))
-            (loop n)))))
-" | ol -q 2> tmp/tcp-$$
-
-
-diff -q tmp/stdout-$$ tmp/tcp-$$ || fail "tcp server output differs from stdout output"
+   diff -q tmp/stdout-$$ tmp/tcp-$$ || fail "files differ: tmp/*-$$"
+done
 
 echo -n ") "
 
-jobs &> /dev/null # background job will get wait()ed here and should no longer show up
-
-pkill -9 %1 2>&1 && fail "Radamsa was left running"
-
-rm tmp/*$$
+rm tmp/*-$$
 
 true
