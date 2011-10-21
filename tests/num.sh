ea57e6eb8acb957077ff35667488edf0681c5b14
Aki Helin <aki.helin@iki.fi>
2011-10-21 15:10:59 +0300

    added a few basic missing byte-level mutations

--- tests/num.sh
+++ tests/num.sh
@@ -4,7 +4,7 @@ echo -n "("
 while true
 do
    echo -n "-"
-   echo "199 + 199" | $@ -t num | grep -q 200 && break
+   echo "254 + 256 + 1 + 2" | $@ -t num | grep -q 255 && break
 done
 
 echo -n ") "
