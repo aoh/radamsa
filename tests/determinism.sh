77b7aaa68c8b72c89baac1039fbda273839f62bc
Aki Helin <aki.helin@iki.fi>
2011-12-06 19:04:06 +0200

    removed checksumming from a unit test to work in BSDs as such and back to prefixing everything to /usr

--- tests/determinism.sh
+++ tests/determinism.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 NFILES=10
 SAMPLES=tests/*
@@ -12,10 +12,10 @@ echo -n "("
 for round in $(ol -e '(iota 0 1 20)')
 do
    echo -n "-"
-   SEED=$RANDOM # bashism
-   FIRST=`$@ -n $NFILES -o - -s $SEED $SAMPLES | md5sum`
-   SECOND=`$@ -n $NFILES -o - -s $SEED $SAMPLES | md5sum`
+   SEED=`cat /dev/urandom | base64 -w 32  | head -n 1`
+   FIRST=`$@ -n $NFILES -o - -s "$SEED" $SAMPLES > det-$$-1`
+   SECOND=`$@ -n $NFILES -o - -s "$SEED" $SAMPLES > det-$$-2`
 
-   test "$FIRST" = "$SECOND" || fail "sums differ $FIRST $SECOND seed $SEED"
+   cmp det-$$-1 det-$$-2 || fail "sums differ $FIRST $SECOND seed $SEED"
 done
 echo -n ") "
