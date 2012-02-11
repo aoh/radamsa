c5f45d040f87918fddf0c92ec763f305df3e8164
Aki Helin <aki.helin@iki.fi>
2012-02-11 17:53:00 +0200

    planning 0.3

--- radamsa-0.3.scm
+++ radamsa-0.3.scm
@@ -4,6 +4,28 @@
 ;;; Radamsa 
 ;;;
 
+;;;
+;;; Todo vs 0.2.x
+;;;
+; 
+; - incremental priority learning
+; - support for incremental mutation learning
+; - multiple mutation attempts on failures
+; - proper per-module documentation (via command line and otherwise)
+; - much more mutations, patterns and generators
+; 
+; mutator:  rs ll meta → muta' rs' ll' meta'    -- merged mutator
+;   - contains the individual mutations and priorities?
+; pattern: rs ll mutator meta → 
+;   a: have fd there and write as you go returning rs' meta' 
+;   b: store updated states to end of stream and pop them after writing
+; generator: rs dict args → (ll ...) 
+;   - unlike in 0.2, make stdin a separate generator to allow separate priority
+;   - file streamers ignore it, it either preloads or streams 
+;   - turn muxing into separate generators because
+;     + rarely useful in practice (incremental learning can do most of the requred stuff already)
+;     + simplifies mental model a lot (stream data -> choose where to do stuff -> do stuff there)
+
 (import (owl args))
 
 (define version-str "Radamsa 0.3a") ;; aka funny fold
