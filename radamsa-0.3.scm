c8a6f10f469516d10b72b673fd565f896a325b93
Aki Helin <aki.helin@iki.fi>
2012-02-16 22:46:50 +0200

    added parts of command line processing. was about to add more but EINTR.

--- radamsa-0.3.scm
+++ radamsa-0.3.scm
@@ -5,26 +5,20 @@
 ;;;
 
 ;; todo and think tomorrow:
-; - convert patterns and outputs to rs → self rs val ...
 ; - make sure it's easy to add documentation and metadata everywhere from beginning
-; - use direct IO later. requires only local changes.
 ; - log-growing block sizes up to max running speed?
-; - would be easy to allow dumping smallish resumable (fasl) states. useful?
-; - allow saving metadata from the beginning
-;   + separately given file? --metadata out.meta
-;   + per file? -M | --metadata-suffix .meta
 
 (import (owl args))
 
 (define version-str "Radamsa 0.3a") ;; aka funny fold
-(define usage-text "Usage: radamsa [args]")
+(define usage-text "Usage: radamsa [arguments] [file ...]")
 
 ;; muta :: rs ll meta → self' rs' ll' meta'
 (define (dummy-mutator rs ll meta)
    (values
       dummy-mutator
       rs
-      (cons (vector 42 42 42) ll)
+      (cons (list->vector (cons #\X (vector->list (car ll)))) (cdr ll))
       (put meta 'dummies 
          (+ 1 (get meta 'dummies 0)))))
 
@@ -40,8 +34,19 @@
 ;; gen :: rs → gen' rs' ll
 (define (dummy-generator dict paths)
    (define (gen rs)
-      (values gen rs 
-         (list (vector 97 97 97) (vector 98 98 98) (vector 99 99 99) (vector 10))))
+      (lets
+         ((rs seed (rand rs #xffffffffffff)))
+         (values gen rs
+            (let loop ((rs (seed->rands seed)))
+               (lets ((rs n (rand rs 10)))
+                  (if (eq? n 0)
+                     null
+                     (lets
+                        ((rs lst (random-numbers rs 26 n))  ;; get some alphabet posns
+                         (lst (map (λ (x) (+ x #\a)) lst))) ;; scale to a-z
+                        (pair
+                           (list->vector (cons #\space lst))
+                           (loop rs)))))))))
    gen)
 
 ;; output :: (ll' ++ (#(rs mutator meta))) fd → rs mutator meta (n-written | #f), handles port closing &/| flushing
@@ -61,30 +66,6 @@
             (else
                (error "output: bad node: " x))))))
 
-;; dict args → rval
-(define (start-radamsa dict paths)
-   ;; show command line stuff
-   (for-each 
-      (λ (thing) (print* (list (car thing) ": " (cdr thing))))
-      (ff->list 
-         (put dict 'samples paths)))
-
-   ;; run test
-   (lets
-      ((rs (seed->rands (time-ms)))
-       (gen (dummy-generator dict paths))
-       (out (get dict 'output 'bug))
-       (rs gen ll (gen rs))
-       (muta dummy-mutator)
-       (pat  dummy-pattern)
-       (out fd meta (out))
-       (rs muta meta n (output (pat rs ll muta meta) fd)))
-      (show " -> meta " meta)
-      (show " => " n))
-
-   ;; done
-   0)
-
 (define (stdout-stream)
    (values stdout-stream stdout 
       (put #false 'output 'stdout))) 
@@ -97,6 +78,55 @@
          (show "I can't yet output " arg)
          #false)))
 
+(define (selection->priority lst)
+   (let ((l (length lst)))
+      (cond
+         ((= l 2) ; (name pri-str)
+            (let ((pri (string->number (cadr lst) 10)))
+               (cond
+                  ((not pri)
+                     (print*-to (list "Bad priority: " (cadr lst)) stderr)
+                     #false)
+                  ((< pri 0) ;; allow 0 to set a fuzzer off
+                     (print*-to (list "Inconceivable: " (cadr lst)) stderr)
+                     #false)
+                  (else
+                     (cons (car lst) pri)))))
+         ((= l 1)
+            (cons (car lst) 1))
+         (else
+            (print*-to (list "Too many things: " lst) stderr)
+            #false))))
+
+(define (test-mutation a b c) a)
+
+(define (name->fuzzer str)
+   (cond
+      ((equal? str "test")
+         test-mutation)
+      (else
+         (print*-to (list "Unknown mutation: " str) stderr)
+         #false)))
+
+;; #f | (name . priority) → #f | (priority . func)
+(define (priority->fuzzer node)
+   (cond
+      ((not node) #false)
+      ((name->fuzzer (car node)) => 
+         (λ (func) (cons func (cdr node))))
+      (else #false)))
+
+(define (car> a b) (> (car a) (car b)))
+
+(define (string->fuzzers str)
+   (lets
+      ((ps (map c/=/ (c/,/ str))) ; ((name [priority-str]) ..)
+       (ps (map selection->priority ps))
+       (fs (map priority->fuzzer ps)))
+      (if (all self fs) 
+         (sort car> fs)
+         #false)))
+
 (define command-line-rules
    (cl-rules
       `((help "-h" "--help" comment "Show this thing.")
@@ -104,8 +134,75 @@
             comment "Where to write the generated data?")
         (count "-n" "--count" cook ,string->integer check ,(λ (x) (> x 0))
             default "1" comment "How many outputs to generate?")
-        (seed "-s" "--seed" has-arg comment "Random seed (any string).")
+        (seed "-s" "--seed" cook ,string->integer comment "Random seed (number, default random)")
+        (fuzzers "-f" "--fuzzers" cook ,string->fuzzers 
+            comment "Enabled mutations and initial probabilities"
+            default "b*=1,l*=2")
+        (list "-l" "--list" comment "List mutations, patterns and generators")
         (version "-V" "--version" comment "Show version information."))))
+
+;; () → string
+(define (urandom-seed)
+   (let ((fd (open-input-file "/dev/urandom"))) ;; #false if not there
+      (if fd
+         (let ((data (interact fd 10)))
+            (close-port fd)
+            (if (vector? data)
+               (vec-fold (λ (n d) (+ d (<< n 8))) 0 data)
+               #false))
+         #false)))
+
+;; () → string (decimal number)
+(define (time-seed)
+   (time-ms))
+
+;; dict args → rval
+(define (start-radamsa dict paths)
+   ;; show command line stuff
+   (cond
+      ((null? paths)
+         ;; fuzz stdin when called as $ cat foo | radamsa | bar -
+         (start-radamsa dict (list "-")))
+      ((not (getf dict 'seed))
+         ;; get a random seed, prefer urandom
+         (start-radamsa 
+            (put dict 'seed (or (urandom-seed) (time-seed)))
+            paths))
+      ((getf dict 'help)
+         (print usage-text)
+         (print-rules command-line-rules)
+         0)
+      ((getf dict 'list)
+         (print "Would list stuff here.")
+         0)
+      (else
+         (print (seed->rands (getf dict 'seed)))
+         ;; print command line stuff
+         (for-each 
+            (λ (thing) (print* (list (car thing) ": " (cdr thing))))
+            (ff->list 
+               (put dict 'samples paths)))
+         
+         (lets/cc ret
+            ((fail (λ (why) (print why) (ret 1)))
+             (rs (seed->rands (getf dict 'seed))))
+            (let loop 
+               ((rs rs)
+                (gen (dummy-generator dict paths))
+                (out (get dict 'output 'bug))
+                (n (getf dict 'count)))
+               (if (= n 0)
+                  0
+                  (lets
+                     ((gen rs ll (gen rs)) 
+                      (muta dummy-mutator)
+                      (pat  dummy-pattern)
+                      (out fd meta (out))
+                      (rs muta meta n-written 
+                        (output (pat rs ll muta meta) fd)))
+                     ;(show " -> meta " meta)
+                     ;(show " => " n-written)
+                     (loop rs gen out (- n 1)))))))))
 
 (λ (args)
    (process-arguments (cdr args) 
