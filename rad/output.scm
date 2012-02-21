5c91918988c3101a7352e87b1515f4e6c349d362
Aki Helin <aki.helin@iki.fi>
2012-02-21 10:33:48 +0200

    added file output

--- rad/output.scm
+++ rad/output.scm
@@ -34,13 +34,41 @@
 
       (define (stdout-stream meta)
          (values stdout-stream stdout 
-            (put meta 'output 'stdout))) 
+            (put meta 'output 'stdout)))
+
+      (define (file-writer pat)
+         (define (gen meta)
+            (lets 
+               ((path ;; <- could also just regex it there
+                  (runes->string
+                     (str-foldr
+                        (λ (char tl)
+                           (cond
+                              ((null? tl)
+                                 (cons char tl))
+                              ((eq? char #\%)
+                                 (case (car tl)
+                                    ((#\n) (render (get meta 'nth 0) (cdr tl)))
+                                    (else (error "Unknown pattern in output path: " (list->string (cons char tl))))))
+                              (else (cons char tl))))
+                        null pat)))
+                (port (open-output-file path)))
+               (if (not port)
+                  (print*-to (list "Warning: cannot write to '" path "'") stderr))
+               (values gen port (put (put meta 'output 'file-writer) 'path path))))
+         gen)
 
       ;; args → (out :: → out' fd meta) v null | #false
-      (define (string->outputs arg)
-         (if (equal? arg "-")
-            stdout-stream
-            (begin
-               (show "I can't yet output " arg)
-               #false)))
+      (define (string->outputs str)
+         (cond
+            ((equal? str "-") ;; conventional way to ask for standard output (and input)
+               stdout-stream)
+            ((m/^:[0-9]+$/ str)
+               (print "This version doesn't yet have TCP server mode.")
+               #false)
+            ((m/^[0-9]{1,3}(\.[0-9]{1,3}){3}:[0-9]+$/ str)
+               (print "This version doesn't yet have TCP client mode.")
+               #false)
+            (else
+               (file-writer str))))
 ))
