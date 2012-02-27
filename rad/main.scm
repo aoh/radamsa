e10d428ecdaa25524fced7c7eb466f0d9afc0230
Aki Helin <aki.helin@iki.fi>
2012-02-27 10:50:41 +0200

    doing verbosity output also with meta logger

--- rad/main.scm
+++ rad/main.scm
@@ -72,20 +72,39 @@
          (print " file: read data from given files")
          (print " random: generate random data"))
 
-      (define (maybe-meta-logger path fail)
-         (if path
-            (let ((port (if (equal? path "-") stdout (open-output-file path))))
-               (if port
-                  (λ (stuff)
-                     (if (eq? stuff 'close)
-                        (if (not (eq? port stdout)) (close-port port))
-                        (mail port (serialize stuff '(10)))))
-                  (fail "Cannot open metadata log file")))
-            (λ (stuff) stuff)))
+      ;; drop B like ls -h
+      (define (verbose-size n)
+         (define (verb n u)
+            (if (or (< n 1024) (null? (cdr u)))
+               (render n (render (car u) null))
+               (verb (div n 1024) (cdr u))))
+         (list->string
+            (verb n '("" "K" "M" "T" "P"))))
+
+      (define (maybe-meta-logger path verbose? fail)
+         (define verb 
+            (if verbose? 
+               (λ (x) (if (ff? x) (print*-to (list " - " (get x 'path "output") ": " (verbose-size (get x 'length 0))) stderr)))
+               (λ (x) x)))
+         (cond
+            (path
+               (let ((port (if (equal? path "-") stdout (open-output-file path))))
+                  (if port
+                     (λ (stuff)
+                        (if (eq? stuff 'close)
+                           (if (not (eq? port stdout)) (close-port port))
+                           (mail port (serialize stuff '(10))))
+                        (verb stuff))
+                     (fail "Cannot open metadata log file"))))
+            (verbose?
+               verb)
+            (else 
+               (λ (x) x))))
 
       (define (maybe-printer verbose)
          (if verbose
-            (λ (args) (print*-to args stderr))
+            (λ (args) 
+               (print*-to args stderr))
             (λ (args) args)))
 
       ;; dict args → rval
@@ -114,13 +133,17 @@
                (lets/cc ret
                   ((fail (λ (why) (print why) (ret 1)))
                    (rs (seed->rands (getf dict 'seed)))
-                   (record-meta (maybe-meta-logger (getf dict 'metadata) fail))
-                   (verb (maybe-printer (getf dict 'verbose)))
+                   (record-meta 
+                     (maybe-meta-logger 
+                        (getf dict 'metadata)
+                        (getf dict 'verbose)
+                        fail))
                    (n (getf dict 'count))
                    (gen 
                      (generator-priorities->generator rs
                         (getf dict 'generators) paths fail (getf dict 'count))))
-                  (record-meta (list 'seed (getf dict 'seed)))
+                  (record-meta 
+                     (list 'seed (getf dict 'seed)))
                   (let loop 
                      ((rs rs)
                       (muta (getf dict 'mutations))
@@ -137,9 +160,8 @@
                             (out fd meta (out meta))
                             (rs muta meta n-written 
                               (output (pat rs ll muta meta) fd))
-                           (meta (put meta 'length n-written)))
-                           (record-meta (ff->list meta))
-                           (verb (list "Made " meta))
+                            (meta (put meta 'length n-written)))
+                           (record-meta meta)
                            (loop rs muta pat out (+ p 1)))))))))
 
       (define (radamsa args)
