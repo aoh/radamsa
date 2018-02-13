1d3a806968a7b4da48f66468a1c2c776a96795d8
Aki Helin <aki.helin@iki.fi>
2018-02-13 10:47:49 +0200

    removed duplicate filters from frequency based mutation tests

--- tests/xpn.sh
+++ tests/xpn.sh
@@ -2,5 +2,5 @@
 
 # check that xml attributes get likely desired number values occasionally
 
-echo '<foo bar=42><baz quux=42>' | $@ -m xp -p od -n 2000 | grep -q 6553 || exit 1
+echo '<foo bar=42><baz quux=42>' | $@ -C 0 -m xp -p od -n 2000 | grep -q 6553 || exit 1
 
